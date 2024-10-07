1ff3e80b358a35120c201ed09a7339861a245f00
Keshav Madhav <keshav2552003@gmail.com>
2024-10-07 19:21:57 +0530

    Added User schema Added routes for login signup and logout

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
 
+import authRoutes from "./routes/auth.router.js";
+
+dotenv.config();
 const app = express();
+const port = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.use('/api/auth', authRoutes)
+
+app.listen(port, () => {
+  connectDB();
+  console.log("Server is running on port", port);
 });
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
-});
