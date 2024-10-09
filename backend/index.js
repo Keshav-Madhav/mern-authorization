49547f48345fa199e95024be1a690423afb2e674
Keshav Madhav <keshav2552003@gmail.com>
2024-10-09 23:24:44 +0530

    modified to be build ready

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,34 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connectDB.js";
-import cookieParser from "cookie-parser";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.router.js";
 
 dotenv.config();
+
 const app = express();
-const port = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(express.json()); // middleware to parse JSON data
-app.use(cookieParser())
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
-app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true
-}))
+app.use(express.json()); // allows us to parse incoming requests:req.body
+app.use(cookieParser()); // allows us to parse incoming cookies
 
-app.use('/api/auth', authRoutes)
+app.use("/api/auth", authRoutes);
 
-app.listen(port, () => {
-  connectDB();
-  console.log("Server is running on port", port);
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server is running on port: ", PORT);
 });
-
