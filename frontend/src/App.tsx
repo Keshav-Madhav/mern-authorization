53dfc3ecc7edd11d64b0d51604008734be5fda77
Keshav Madhav <keshav2552003@gmail.com>
2024-10-09 11:48:54 +0530

    Created verify email with otp page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import FloatingShape from "./components/FloatingShape"
 import SignUpPage from "./pages/SignUpPage"
 import LoginPage from "./pages/LoginPage"
+import VerifyEmailPage from "./pages/VerifyEmailPage"
 
 function App() {
   return (
@@ -35,7 +36,7 @@ function App() {
         <Route path="/" element={"Home"} />
         <Route path="signup" element={<SignUpPage/>} />
         <Route path="login" element={<LoginPage/>} /> 
-
+        <Route path="verify-email" element={<VerifyEmailPage/>} />
       </Routes>
     </div>
   )
